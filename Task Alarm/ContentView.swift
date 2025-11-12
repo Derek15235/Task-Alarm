2f9227eab86aa5c6db8a8802b79f308b3db91d92
Derek15235 <derekdjain@gmail.com>
2025-11-12 00:40:00 -0800

    Testing out current AlarmKit functionality

--- Task Alarm/ContentView.swift
+++ Task Alarm/ContentView.swift
@@ -6,18 +6,103 @@
 //
 
 import SwiftUI
+import AlarmKit
 
 struct ContentView: View {
+    @State private var isAuthorized: Bool = false
+    @State private var scheduleDate: Date = .now
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Group {
+                if isAuthorized {
+                    AlarmView()
+                } else {
+                    Text("You need to allow alarms in settings to use this app")
+                        .multilineTextAlignment(.center)
+                        .padding(10)
+                        .glassEffect()
+                    
+                }
+            }
+            .navigationTitle("AlarmKit")
         }
-        .padding()
+        .task {
+            do {
+                try await checkAndAuthorize()
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
     }
+    
+    @ViewBuilder
+    private func AlarmView() -> some View {
+        List {
+            Section("Date & Time") {
+                DatePicker("", selection: $scheduleDate, displayedComponents: [.date, .hourAndMinute])
+                    .labelsHidden()
+            }
+            
+            Button("Set Alarm") {
+                Task {
+                    do {
+                        try await setAlarm()
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+            
+        }
+    }
+    
+    private func setAlarm() async throws{
+        // Alert
+        let alert = AlarmPresentation.Alert(
+            title: "Time's Up!!!",
+            stopButton: .init(text: "Stop", textColor: .red, systemImageName: "stop.fill")
+        )
+        
+        // Presentation
+        let presentation = AlarmPresentation(alert: alert)
+        
+        // Attributes
+        let attributes = AlarmAttributes<CountDownAttribute>(presentation: presentation, metadata: .init(), tintColor: .orange)
+        
+        // Schedule
+        let schedule = Alarm.Schedule.fixed(scheduleDate)
+        
+        // Configuration
+        let config = AlarmManager.AlarmConfiguration(
+            schedule: schedule,
+            attributes: attributes
+        )
+        
+        let id = UUID()
+        
+        let _ = try await AlarmManager.shared.schedule(id: id, configuration: config)
+        print("Alarm Set Successfully")
+    }
+    
+    
+    private func checkAndAuthorize() async throws {
+        switch AlarmManager.shared.authorizationState {
+        case .notDetermined:
+            /// Requesting for authorization
+            let status = try await AlarmManager.shared.requestAuthorization()
+            isAuthorized = status == .authorized
+        case .denied:
+            isAuthorized = false
+        case .authorized:
+            isAuthorized = true
+        @unknown default:
+            fatalError()
+        }
+    }
+    
+    
 }
+
 
 #Preview {
     ContentView()
---
+++ Task Alarm/Helpers/CountDownAttribute.swift
@@ -0,0 +1,13 @@
+//
+//  CountDownAttribute.swift
+//  Task Alarm
+//
+//  Created by Derek Jain on 11/12/25.
+//
+
+import SwiftUI
+import AlarmKit
+
+struct CountDownAttribute: AlarmMetadata, Hashable, Sendable {
+    // Custom Properties Here
+}
