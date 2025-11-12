13e8d8ef0a8482d2aeaf943f50c7036e249452c7
Derekdja <derekdjain@gmail.com>
2025-11-11 22:50:23 -0800

    Initial Commit

---
+++ Task Alarm/Task_AlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_AlarmApp.swift
+//  Task Alarm
+//
+//  Created by Derek Jain on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Task_AlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
